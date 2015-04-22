6230596513b5d04a19683d28f992c87665bcffef
Nils <nils.backstrom.2343@gmail.com>
2015-04-22 16:51:32 +0200

    Finputs på task2

--- project2/perloc_cummulative.m
+++ project2/perloc_cummulative.m
@@ -1,12 +1,8 @@
-%Task 2
-
 clear all
 close all
 
 L = 200;
-%L = 800;
 pvals = [0.3, 0.592, 0.8];
-
 Q = zeros(length(pvals), L*L);
 
 i = 1;
@@ -18,25 +14,28 @@ for p=pvals
     % Picking indices != 0 for cumsum and plotting
     idx = find(Q(i,:) > 0);
     
-    % Plotting distribution in standard plot
+    % Plotting distribution in standard plot and loglog
     f1 = figure(1);
     plot(idx, Q(i,idx), '*', ...
         'DisplayName' , ['p=' num2str(p)]);
+    set(gca, 'FontSize', 16)
     hold on
     
+    lh1 = legend('-DynamicLegend');
     xlabel('Cluster size', 'FontSize', 20);
     ylabel('Frequency', 'FontSize', 20);
-    legend('-DynamicLegend');
+    set(lh1, 'FontSize', 16);
     
-    % Plotting distribution in log-log plot
     f2 = figure(2);
     loglog(idx,Q(i,idx), '*', ...
         'DisplayName', ['p=' num2str(p)]);
+    set(gca, 'FontSize', 16)
     hold on
     
+    lh2 = legend('-DynamicLegend');
     xlabel('Cluster size', 'FontSize', 20);
     ylabel('Frequency', 'FontSize', 20);
-    legend('-DynamicLegend');
+    set(lh2, 'FontSize', 16);
     
     % Perform cummulative sum
     Q(i,:) = cumsum(Q(i,:), 'reverse');
@@ -45,13 +44,15 @@ for p=pvals
     f3 = figure(3);
     loglog(idx,Q(i,idx), '*', ...
         'DisplayName', ['p=' num2str(p)]);
+    set(gca, 'FontSize', 16)
     hold on
     
+    lh3 = legend('-DynamicLegend');
     xlabel('Cluster size', 'FontSize', 20);
     ylabel('Frequency', 'FontSize', 20);
-    legend('-DynamicLegend');
+    set(lh3, 'FontSize', 16);
     
     i = i + 1;
 end
 
-hold off
+

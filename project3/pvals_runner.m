de5caa60e6c383b188bb1fa689c8b020d762fc9d
Nils Backstrom <nils.backstrom.2343@gmail.com>
2015-05-19 17:13:38 +0200

    added data saving

--- project3/pvals_runner.m
+++ project3/pvals_runner.m
@@ -1,22 +1,42 @@
 T = 1.9;
 R = 1;
+P = 0;
 S = 0;
 
-n = 40;
+L = 100;
 steps = 100;
 p = 0.1;
 
+t_min = 1.7;
+t_max = 2.1;
 n_vals = 10;
-Pvals = linspace(0,0.5,n_vals);
+Tvals = linspace(t_min,t_max,n_vals);
 
 dens_data = zeros(n_vals, steps+1);
-grid_data = zeros(n_vals, n, n);
+grid_data = zeros(n_vals, L, L);
 
-for k=1:length(Pvals)
+data = struct;
 
-    rewards = [R S T Pvals(k)];
-    [dens_data(k,:), grid_data(k,:,:)] = prisoner_spatial(rewards, n, steps, p, true);
+f1 = figure;
+for k=1:length(Tvals)
+
+    rewards = [R S Tvals(k) P];
+    [dens_data(k,:), grid_data(k,:,:)] = prisoner_spatial(rewards, L, steps, p);
     
+    f1;
+    plot(dens_data(k,:), 'DisplayName', num2str(Tvals(k)))
+    hold on
+    k
 end
 
-plot(dens_data')
+lh = legend('-DynamicLegend');
+
+data.dens_data = dens_data;
+data.grid_data = grid_data;
+
+filename = ['pd_tvals=' num2str(t_min) '-' num2str(t_max) ...
+            '_nvals=' num2str(n_vals) '_' ...
+            'L=' num2str(L) '_steps=' num2str(steps), '_p=' num2str(p)];
+        
+filename = strrep(filename, '.', ',');
+save(filename, 'data');

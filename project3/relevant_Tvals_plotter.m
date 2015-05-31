01a20203d059043f1d31f8b373c5559afca1f8b7
Nils Backstrom <nils.backstrom.2343@gmail.com>
2015-05-31 14:43:10 +0200

    Runs relevant Tvals for different initial p. Also plot results in bar graph to distinguish where initial conditions matters.

---
+++ project3/relevant_Tvals_plotter.m
@@ -0,0 +1,102 @@
+% Clear all and close all before running. Produces plots from datafiles
+% with all chosen p. Has "hold on" on figures.
+
+%% 
+% pstr = num2str(p);
+% pstr = strrep(pstr,'.',',');
+
+p1 = load('data/pd_relevant_tvals_L=200_steps=200_p=0,1.mat');
+p4 = load('data/pd_relevant_tvals_L=200_steps=200_p=0,4.mat');
+p7 = load('data/pd_relevant_tvals_L=200_steps=200_p=0,7.mat');
+
+p = [p1.data p4.data p7.data];
+%close all
+
+n = size(p(1).dens_data);
+n = n(1)
+L = 200;
+len = size(p(1).dens_data);
+len = len(2)
+
+Tvals = [1.4 1.55 1.65 1.78 1.9 2.1];
+pvals = [0.1 0.4 0.7];
+
+f = [];
+
+%% Plot density 
+final_dens = zeros(length(p), n);
+for l=1:length(p)
+    for k=1:n
+        final_dens(l,k) = mean(p(l).dens_data(k, len-25:len));
+    end
+end
+
+f(1) = figure(1);
+x = 1:n;
+y = [final_dens(1,:); final_dens(2,:); final_dens(3,:)]';
+bh1 = bar(x, y);
+set(bh1,{'DisplayName'}, {['p = 0.1']; ['p = 0.4']; ['p = 0.7']})
+legend('-DynamicLegend')
+
+set(gca, 'XTick', 1:n, 'XTickLabel', Tvals, 'FontSize', 16')
+
+xlabel('Defector to Collaborator reward')
+ylabel('Equilibrium C density')
+
+%% Plot cluster counts
+deffector_clusters = zeros(length(p),n);
+cooperator_clusters = zeros(length(p), n);
+for l=1:length(p)
+    for k=1:n
+        grid = reshape(p(l).grid_data(k,:,:), [L L]);
+        [~, deffector_clusters(l,k)] = bwlabel(grid, 8);
+        [~, cooperator_clusters(l,k)] = bwlabel(not(grid), 8);
+    end
+end
+
+f(2) = figure(2);
+x = 1:n;
+y = [cooperator_clusters(1,:); cooperator_clusters(2,:); cooperator_clusters(3,:)]';
+bh2 = bar(x, y);
+
+set(bh2,{'DisplayName'}, {['p = 0.1']; ['p = 0.4']; ['p = 0.7']})
+legend('-DynamicLegend')
+
+set(gca, 'XTick', 1:n, 'XTickLabel', Tvals, 'FontSize', 16')
+
+xlabel('Defector to Collaborator reward')
+ylabel('Number of clusters')
+
+% Show grid
+% figure(3)
+% colormap([0,0,0;1,1,1])
+% for k=1:n
+%     grid = reshape(p(1).grid_data(k,:,:), [L L]);
+%     imagesc(grid, [0 1])
+%     title(['T = ' num2str(Tvals(k))])
+%     drawnow
+%     pause(0.2)
+% end
+figs = [];
+for l=1:length(p)
+    for k=1:n
+        f = figure;
+        colormap([0,0,0;1,1,1])
+        figs = [figs f];
+        grid = reshape(p(l).grid_data(k,:,:), [L L]);
+        imagesc(grid, [0 1])
+        title(['T = ' num2str(Tvals(k)) ', p = ' num2str(pvals(l))])
+        %drawnow
+        %pause(0.2)
+        dir = ['figs/grid_figs/'];
+        figname = ['T=' num2str(Tvals(k)) 'p=' num2str(pvals(l))];
+        figname = strrep(figname, '.', ',');
+        saveas(f, [dir figname],'png')
+        close(f)
+    end
+end
+
+
+
+
+
---
+++ project3/relevant_Tvals_runner.m
@@ -0,0 +1,52 @@
+T = 1.9;
+R = 1;
+P = 0;
+S = 0;
+
+L = 200;
+steps = 200;
+p = 0.7;
+
+%% Tvals
+n_vals = 100;
+t_min = 1.3;
+t_max = 2.3;
+Tvals = linspace(t_min,t_max,n_vals);
+
+% Tvals, one in every region
+Tvals = [1.4 1.55 1.65 1.78 1.9 2.1];
+
+%% pvals
+p = [0.1 0.4 0.7];
+idx = 3;
+
+
+dens_data = zeros(n_vals, steps+1);
+grid_data = zeros(n_vals, L, L);
+
+data = struct;
+
+f2 = figure;
+
+for k=1:length(Tvals)
+
+    rewards = [R S Tvals(k) P];
+    [dens_data(k,:), grid_data(k,:,:)] = prisoner_spatial(rewards, L, steps, p(idx));
+
+    f1;
+    plot(dens_data(k,:), 'DisplayName', num2str(Tvals(k)))
+    hold on
+    disp(['k = ' num2str(k)]);
+end
+
+
+lh = legend('-DynamicLegend');
+
+data.dens_data = dens_data;
+data.grid_data = grid_data;
+
+filename = ['pd_relevant_tvals_L=' num2str(L) '_steps=' ...
+            num2str(steps), '_p=' num2str(p(idx))];
+        
+filename = strrep(filename, '.', ',');
+save(filename, 'data');
